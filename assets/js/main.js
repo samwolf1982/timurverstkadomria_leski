bf1f542b275e1f3111fc3a7118daac521a8431ce
samwolf1982 <biryukwolf@yahoo.com>
2017-03-19 10:58:24 +0200

    cooler ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+  // cooler
     $(document).ready(function(){
 
 var ci=$("#center_image");
@@ -7,12 +8,27 @@ var ci=$("#center_image");
 
 
 $('.border_wrap').on('click',function(event) {
+ // var center=false;
+ if ( $(this).hasClass('border_wrap_center')) {
+      console.log("center");
+     var img= $(this).children('.thumbnail').children('img');
+        console.log(img.attr('id'));
+     // $.fancybox.open('<div class="message"><h2>Hello!</h2><p>You are awesome!</p></div>'); 
+     // $.fancybox.open(img.innerHTML); 
+
+      event.preventDefault();
+      return true;
+ }
+
+
+
+//----------
   var gate=false;
  if ( $(this).hasClass('border_wrap_down')) {
  gate=true;
  }
 
-  event.preventDefault();
+//  event.preventDefault();
   /* Act on the event */
 
   var parent=$(this).parent();
@@ -30,6 +46,14 @@ curent.show();
    
  });
  
+ th.addClass('border_wrap_center');  
+curent.removeClass('border_wrap_center');
+
+
+
+
+
+
  if (gate) {
    console.log('bottoom '+ th.attr('class'));
       console.log('bottoom2 '+ curent.attr('class'));
@@ -44,6 +68,37 @@ curent.show();
    // console.dir();
  }
 
+ // <a href="/assets/img/allimage/l4.jpg" data-fancybox data-caption="">
+ //     <img hidden="true" id="rc-3" src="/assets/img/allimage/4.jpg" width="360" height="240" alt="">
+
+
+  var img_inner=th.find('img');
+  var full_img_path=th.find('img').attr('src');
+  var fileNameIndex = th.find('img').attr('src').lastIndexOf("/") + 1;
+var filename = th.find('img').attr('src').substr(fileNameIndex);
+
+ var new_name=full_img_path.replace(filename,'l'+filename);
+  console.log(new_name);
+  var c_image=curent.find('img');
+  curent.find('a').remove();
+  curent.children('.thumbnail').append(c_image);
+
+ var a_inner= '<a id="dodo" href="'+new_name+'" data-fancybox data-caption=""></a>';
+ // var a_inner = document.createElement('a');
+ // a_inner.append(img_inner);
+  th.children('.thumbnail').html(a_inner);
+  $('#dodo').append(img_inner);
+  // ркбочи код !!!!!!!!!!! вствка только ид
+  // curent.children('.thumbnail').append( $('<a/>')
+  //   .attr("id", "newDiv2")
+  //   .addClass("newDiv purple bloated")
+  //   .append(img_inner)
+  //     );
+
+
+ // curent.children('.thumbnail a').append(img_inner);
+
+
 
  
 });
@@ -53,7 +108,7 @@ curent.show();
 });
 
 });
-
+// end  kooler
 
 
 // molbert
